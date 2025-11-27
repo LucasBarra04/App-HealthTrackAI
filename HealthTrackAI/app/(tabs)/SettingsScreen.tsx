7f75acad61a8710e2c033451a888272257236304
Mateus Ramos <mateusramosv@hotmail.com>
2025-11-27 19:50:45 -0300

    adicionar imports iniciais e estrutura base

---
+++ HealthTrackAI/app/(tabs)/SettingsScreen.tsx
@@ -0,0 +1,12 @@
+import React, { useState, createContext, useContext } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Switch,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const ThemeContext = createContext();
